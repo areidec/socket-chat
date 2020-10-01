5e55d4511692f71820ccd14411772e0394bf718e
Areidec <tatarchuk@grechka.digital>
2020-10-01 21:07:32 +0300

    next

--- client/src/pages/AuthPage/index.js
+++ client/src/pages/AuthPage/index.js
@@ -1,8 +1,32 @@
 import React from 'react';
+import {makeStyles} from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
+
+const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    minHeight: '100vh',
+    minWidth: '100vw',
+
+  }
+});
 
 const AuthPage = () => {
+  const classes = useStyles();
+
+
   return (
-    <>AuthPage</>
+    <div className={classes.root}>
+      <Card>
+        <CardContent>asdsd</CardContent>
+      </Card>
+    </div>
   )
 }
 
